22bacc71a3f0c30ab0e0a42ba01f642ad88d0cb7
JCM <cmathews@rti.org>
2014-04-24 10:15:20 -0400

    BENMAP-22 add try catch to main thread to handle unhandled exceptions

--- BenMAP/Program.cs
+++ BenMAP/Program.cs
@@ -11,15 +11,42 @@ namespace BenMAP
         [STAThread]
         static void Main(string[] arg)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            string strArg = "";
-            foreach (string s in arg)
+            try
             {
-                strArg = strArg + " " + s;
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                string strArg = "";
+                foreach (string s in arg)
+                {
+                    strArg = strArg + " " + s;
+                }
+                CommonClass.InputParams = new string[] { strArg };
+                Application.Run(new Main());
             }
-            CommonClass.InputParams = new string[] { strArg };
-            Application.Run(new Main());
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+
+                //show error reporting form unless error is in error reporting form
+                if (ex.StackTrace.IndexOf("ErrorReporting", StringComparison.OrdinalIgnoreCase) < 0)
+                {
+                    try
+                    {
+                        DialogResult dialogResult = MessageBox.Show("The following error occurred:\n\n" + ex.Message + "\n\nWould you like to report this to the BenMAP-CE development team?", "Error", MessageBoxButtons.YesNo);
+
+                        if (dialogResult == DialogResult.Yes)
+                        {
+                            ErrorReporting frm = new ErrorReporting();
+                            frm.ShowDialog();
+                        }
+                    }
+                    catch (Exception ex2)
+                    {
+                        Logger.LogError(ex2);
+                    }
+                }
+               
+            }
         }
     }
 }
