60034ff7242535188830611c34e05876b23d77d3
JCM <cmathews@rti.org>
2014-06-04 14:25:33 -0400

    benmap-46

--- BenMAP/Tools/GBDRollback.Designer.cs
+++ BenMAP/Tools/GBDRollback.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gbAreaSelection = new System.Windows.Forms.GroupBox();
+            this.gbRollbacks = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
+            // 
+            // gbAreaSelection
+            // 
+            this.gbAreaSelection.Location = new System.Drawing.Point(13, 13);
+            this.gbAreaSelection.Name = "gbAreaSelection";
+            this.gbAreaSelection.Size = new System.Drawing.Size(200, 300);
+            this.gbAreaSelection.TabIndex = 0;
+            this.gbAreaSelection.TabStop = false;
+            this.gbAreaSelection.Text = "Area Selection";
+            // 
+            // gbRollbacks
+            // 
+            this.gbRollbacks.Location = new System.Drawing.Point(13, 319);
+            this.gbRollbacks.Name = "gbRollbacks";
+            this.gbRollbacks.Size = new System.Drawing.Size(874, 220);
+            this.gbRollbacks.TabIndex = 1;
+            this.gbRollbacks.TabStop = false;
+            this.gbRollbacks.Text = "Rollbacks";
             // 
             // GBDRollback
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(908, 551);
+            this.Controls.Add(this.gbRollbacks);
+            this.Controls.Add(this.gbAreaSelection);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "GBDRollback";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -44,5 +66,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbAreaSelection;
+        private System.Windows.Forms.GroupBox gbRollbacks;
     }
 }
