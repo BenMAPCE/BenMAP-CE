f0b4719cc8ed04c317114e933622f79a86048b9f
JCM <cmathews@rti.org>
2014-07-09 10:50:22 -0400

    benmap-50

--- BenMAP/ManageSetup/ManageIncidenceDataSets.cs
+++ BenMAP/ManageSetup/ManageIncidenceDataSets.cs
@@ -122,6 +122,7 @@ namespace BenMAP
                 cboEndpointGroup.DropDownWidth = maxEndpointGroupWidth; cboEndpoint.DropDownWidth = maxEndpointWidth;
                 cboEndpointGroup.SelectedIndex = 0;
                 cboEndpoint.SelectedIndex = 0;
+                btnViewMetadata.Enabled = false;
             }
             catch (Exception ex)
             {
@@ -373,11 +374,33 @@ namespace BenMAP
         {
             _metadataObj = SQLStatementsCommonClass.getMetadata(Convert.ToInt32(_dataSetID), CommonClass.ManageSetup.SetupID);
             _metadataObj.SetupName = CommonClass.ManageSetup.SetupName;//_dataName;//_lstDataSetName;
+            btnViewMetadata.Enabled = false;
             ViewEditMetadata viewEMdata = new ViewEditMetadata(_metadataObj);
             DialogResult dr = viewEMdata.ShowDialog();
             if (dr.Equals(DialogResult.OK))
             {
                 _metadataObj = viewEMdata.MetadataObj;
+            }
+        }
+
+        private void olvIncidenceRates_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sender != null)
+                {
+
+                    BrightIdeasSoftware.DataListView dlv = sender as BrightIdeasSoftware.DataListView;
+
+                    if (dlv.SelectedItem != null)
+                    {
+                        btnViewMetadata.Enabled = true;                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
             }
         }
     }
