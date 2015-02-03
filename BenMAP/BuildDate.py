14e866aefff83c42a93bbab772f81d967b5fc81e
Adam Shelton <adamshelton@rti.org>
2015-02-03 14:36:48 -0500

    Added Prebuild event to generate a build date to be used throughout the program Numerous Capitalization issues fixed Added BuildDate.py script to project directory, Added output resource file BenMAP/Resources/BuildDate.txt

---
+++ BenMAP/BuildDate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import time
+import datetime
+
+i = datetime.datetime.now()
+with open('/Repositories/BenMAP-CE/BenMAP/Resources/BuildDate.txt','w') as f:
+    f.write ( time.strftime("%B") + " %s, %s" %(i.day,i.year))
+
+
