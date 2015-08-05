d95dfc1636f3a94a45b3bd578ee7334e26a7451d
Adam Shelton <adamshelton@rti.org>
2015-08-05 15:04:08 -0400

    Updated AssemblyInfo and release notes version number and setup.vdproj
    Former-commit-id: 49def63b944a3ca455135ceb27087fefa630823a

--- BenMAP/BuildDate.py
+++ BenMAP/BuildDate.py
@@ -3,7 +3,7 @@ import time
 import datetime
 
 i = datetime.datetime.now()
-with open('/Repositories/BenMAP-CE/BenMAP/Resources/BuildDate.txt','w') as f:
+with open('/Repositories/git/BenMAP-CE/BenMAP/Resources/BuildDate.txt','w') as f:
     f.write ( time.strftime("%B") + " %s, %s" %(i.day,i.year))
 
 
