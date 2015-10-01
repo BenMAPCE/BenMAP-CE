4b49d88a4b73b6939e3a613554588d7ef7d6036c
Adam Shelton <adamshelton@rti.org>
2015-10-01 15:04:49 -0400

    Revert "added Country files to installer"
    This reverts commit 69c9bb44dd149868b928deb7bac1d505ca1e39da [formerly 34e1410fca58dfd1cb5b4d28ed87de196abd9df2].
    
    
    Former-commit-id: 49f2a13d0a5381bbf54b383f441ad2f68c2aa838

--- BenMAP/Properties/AssemblyInfo.cs
+++ BenMAP/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("9285e0f8-4591-4b24-b47f-9e103d6fe03d")]
 
 
-[assembly: AssemblyVersion("1.1.7.2")]
-[assembly: AssemblyFileVersion("1.1.7.2")]
+[assembly: AssemblyVersion("1.1.7.1")]
+[assembly: AssemblyFileVersion("1.1.7.1")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject1")]
