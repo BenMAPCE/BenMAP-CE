b6e9f49e00e36f6c7986a5fd01f4ba4deb8c9931
Adam Shelton <adamshelton@rti.org>
2016-03-15 17:04:40 -0400

    updated version #
    Former-commit-id: 9f6218386b80f8dab673bde069b1decda59b5b7e

--- BenMAP/Properties/AssemblyInfo.cs
+++ BenMAP/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("9285e0f8-4591-4b24-b47f-9e103d6fe03d")]
 
 
-[assembly: AssemblyVersion("1.3.1.1")]
-[assembly: AssemblyFileVersion("1.3.1.1")]
+[assembly: AssemblyVersion("1.3.1.2")]
+[assembly: AssemblyFileVersion("1.3.1.2")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject1")]
