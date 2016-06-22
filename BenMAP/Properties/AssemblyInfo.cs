5e014ef4d9d615eb4762fb57fb6af4f609ae79ab
Caitlin Hulsey <chulsey@rti.org>
2016-06-22 10:11:37 -0400

    BenMAP-478 Fixed link and mailto on About page
    Former-commit-id: 4d1023893fa9e13d9ea9557a564bceeec577b564

--- BenMAP/Properties/AssemblyInfo.cs
+++ BenMAP/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 
 
 [assembly: AssemblyTitle("BenMAP-CE 1.3")]
-[assembly: AssemblyDescription("U.S. EPA, 2015. Environmental Benefits Mapping and Analysis Program - Community Edition (Version 1.3), 2015. Research Triangle Park, NC. Available on the Internet at <http://www.epa.gov/air/benmap/>.\r\nBenMAP-CE is free and open source software. Requests for the source code, as well as comments and questions, should be sent to benmap@epa.gov.")]
+[assembly: AssemblyDescription("U.S. EPA, 2015. Environmental Benefits Mapping and Analysis Program - Community Edition (Version 1.3), 2015. Research Triangle Park, NC. Available on the Internet at <http://www.epa.gov/benmap/>.\r\nBenMAP-CE is free and open source software. Requests for the source code, as well as comments and questions, should be sent to mailto:benmap@epa.gov.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Environmental Benefits Mapping and Analysis Program - Community Edition (BenMAP-CE)")]
