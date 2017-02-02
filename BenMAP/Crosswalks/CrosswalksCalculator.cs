89a8a57d1da5160b555e967ce09e1cd43a7bfc11
mogikanin <mogikanin.tir@gmail.com>
2017-02-02 16:51:23 +0300

    Remove try\catch

--- BenMAP/Crosswalks/CrosswalksCalculator.cs
+++ BenMAP/Crosswalks/CrosswalksCalculator.cs
@@ -68,117 +68,21 @@ namespace BenMAP
             }
         }
 
-        public static Dictionary<CrosswalkIndex, CrosswalkRatios> Calculate(IFeatureSet grid, IFeatureSet features, CancellationToken cancellationToken, IProgress progress)
+        public static Dictionary<CrosswalkIndex, CrosswalkRatios> Calculate(IFeatureSet grid, IFeatureSet features,
+            CancellationToken cancellationToken, IProgress progress)
         {
-            try
-            {
-                if (grid == null) throw new ArgumentNullException("grid");
-                if (features == null) throw new ArgumentNullException("features");
-                if (progress == null) throw new ArgumentNullException("progress");
 
-                var output = new Dictionary<CrosswalkIndex, CrosswalkRatios>();
-                var sync_lock = new object();
-
-                var featuresCount = !features.IndexMode ? features.Features.Count : features.ShapeIndices.Count;
-                var cellsCount = !grid.IndexMode ? grid.Features.Count : grid.ShapeIndices.Count;
-
-                progress.OnProgressChanged("Preparing data", 0);
-
-                // Sort grid cells
-                var cellsX = new List<GridCell>();
-                var cellsY = new List<GridCell>();
-
-                for (int i = 0; i < cellsCount; i++)
-                {
-                    var cellGeometry = grid.GetShape(i, false).ToGeometry();
-                    var env = cellGeometry.EnvelopeInternal;
-
-                    cellsX.Add(new GridCell(cellGeometry, env.MinX, env.MaxX, i));
-                    cellsY.Add(new GridCell(cellGeometry, env.MinY, env.MaxY, i));
-                }
-
-                var sortedByX = cellsX.OrderBy(_ => _.Min).ToList();
-                var sortedByY = cellsY.OrderBy(_ => _.Min).ToList();
-
-                var po = new ParallelOptions { CancellationToken = cancellationToken, MaxDegreeOfParallelism = -1 };
-                var currentCount = 0;
-
-                // Begin calculation
-                progress.OnProgressChanged("Begin calculation", 0);
-                Parallel.ForEach(features.Features, po, delegate(IFeature feature)
-                {
-                    var featureGeometry = feature.Geometry;
-                    var featureId = feature.Fid;
-                    var featureArea = featureGeometry.Area;
-
-                    var featureEnvelope = featureGeometry.EnvelopeInternal;
-                    var filterByX = Find(sortedByX, featureEnvelope.MinX, featureEnvelope.MaxX);
-                    var filterByY = Find(sortedByY, featureEnvelope.MinY, featureEnvelope.MaxY);
-                    var intersectionCells = filterByX.Intersect(filterByY, new GridCellEqualityComparer());
-
-                    var localList = new List<Tuple<CrosswalkIndex, CrosswalkRatios>>();
-                    foreach (var cell in intersectionCells)
-                    {
-                        var cellId = cell.Fid;
-                        var cellGeometry = cell.Geometry;
-
-                        var intersection = cellGeometry.Intersection(featureGeometry);
-                        if (!(intersection == null || intersection.IsEmpty))
-                        {
-                            var area1 = cell.Area;
-                            var intersectionArea = intersection.Area;
-
-                            var entry = new CrosswalkRatios
-                            {
-                                ForwardRatio = (float)(intersectionArea / area1),
-                                BackwardRatio = (float)(intersectionArea / featureArea)
-                            };
-
-                            var index = new CrosswalkIndex
-                            {
-                                FeatureId1 = cellId,
-                                FeatureId2 = featureId
-                            };
-
-                            // Grid cell fully in feature, exclude it from further calcuations
-                            if (entry.ForwardRatio == 1.0f)
-                            {
-                                cell.Used = true;
-                            }
-                            localList.Add(new Tuple<CrosswalkIndex, CrosswalkRatios>(index, entry));
-                        }
-                    }
-
-                    lock (sync_lock)
-                    {
-                        localList.ForEach(_ => output.Add(_.Item1, _.Item2));
-                    }
-
-                    var current = Interlocked.Increment(ref currentCount);
-                    progress.OnProgressChanged(string.Format("Feature {0}/{1} finished", current, featuresCount), current / (featuresCount * 1.0f) * 100.0f);
-                });
-
-                progress.OnProgressChanged(string.Format("Finished. Total crosswalks: {0}", output.Count), 100);
-                return output;
-            }
-            catch
-            {
-                return null;
-            }
-
-        }
-
-
-        public static Dictionary<CrosswalkIndex, CrosswalkRatios> Calculate2(IFeatureSet grid, IFeatureSet features, CancellationToken cancellationToken)
-        {
             if (grid == null) throw new ArgumentNullException("grid");
             if (features == null) throw new ArgumentNullException("features");
+            if (progress == null) throw new ArgumentNullException("progress");
 
             var output = new Dictionary<CrosswalkIndex, CrosswalkRatios>();
             var sync_lock = new object();
 
             var featuresCount = !features.IndexMode ? features.Features.Count : features.ShapeIndices.Count;
             var cellsCount = !grid.IndexMode ? grid.Features.Count : grid.ShapeIndices.Count;
+
+            progress.OnProgressChanged("Preparing data", 0);
 
             // Sort grid cells
             var cellsX = new List<GridCell>();
@@ -196,10 +100,11 @@ namespace BenMAP
             var sortedByX = cellsX.OrderBy(_ => _.Min).ToList();
             var sortedByY = cellsY.OrderBy(_ => _.Min).ToList();
 
-            var po = new ParallelOptions { CancellationToken = cancellationToken, MaxDegreeOfParallelism = -1 };
+            var po = new ParallelOptions {CancellationToken = cancellationToken, MaxDegreeOfParallelism = -1};
             var currentCount = 0;
 
             // Begin calculation
+            progress.OnProgressChanged("Begin calculation", 0);
             Parallel.ForEach(features.Features, po, delegate(IFeature feature)
             {
                 var featureGeometry = feature.Geometry;
@@ -225,8 +130,8 @@ namespace BenMAP
 
                         var entry = new CrosswalkRatios
                         {
-                            ForwardRatio = (float)(intersectionArea / area1),
-                            BackwardRatio = (float)(intersectionArea / featureArea)
+                            ForwardRatio = (float) (intersectionArea / area1),
+                            BackwardRatio = (float) (intersectionArea / featureArea)
                         };
 
                         var index = new CrosswalkIndex
@@ -250,8 +155,11 @@ namespace BenMAP
                 }
 
                 var current = Interlocked.Increment(ref currentCount);
+                progress.OnProgressChanged(string.Format("Feature {0}/{1} finished", current, featuresCount),
+                    current / (featuresCount * 1.0f) * 100.0f);
             });
 
+            progress.OnProgressChanged(string.Format("Finished. Total crosswalks: {0}", output.Count), 100);
             return output;
         }
     }
