77d112d8befc8b9c69ed80ce2d62e79e178ea5e9
Caitlin Hulsey <chulsey@rti.org>
2017-02-10 11:56:57 -0500

    Updated date year and assembly version

--- BenMAP/Properties/AssemblyInfo.cs
+++ BenMAP/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 
 
 [assembly: AssemblyTitle("BenMAP-CE 1.4")]
-[assembly: AssemblyDescription("U.S. EPA, 2015. Environmental Benefits Mapping and Analysis Program - Community Edition (Version 1.4), 2015. Research Triangle Park, NC. Available on the Internet at <http://www.epa.gov/air/benmap/>.\r\nBenMAP-CE is free and open source software. Requests for the source code, as well as comments and questions, should be sent to benmap@epa.gov.")]
+[assembly: AssemblyDescription("U.S. EPA, 2017. Environmental Benefits Mapping and Analysis Program - Community Edition (Version 1.4), 2017. Research Triangle Park, NC. Available on the Internet at <http://www.epa.gov/air/benmap/>.\r\nBenMAP-CE is free and open source software. Requests for the source code, as well as comments and questions, should be sent to benmap@epa.gov.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Environmental Benefits Mapping and Analysis Program - Community Edition (BenMAP-CE)")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("9285e0f8-4591-4b24-b47f-9e103d6fe03d")]
 
 
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.4.2.3")]
+[assembly: AssemblyFileVersion("1.4.2.3")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject1")]
