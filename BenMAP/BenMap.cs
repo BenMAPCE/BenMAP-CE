f36538b54d9c7ef7fa1e1df12884364234a0bbb2
DANAMES\Dan <dan.ames@byu.edu>
2017-08-16 02:06:57 -0600

    Improved handling of no data symbology by removing the features completely that don't have data. This also speeds drawing.

--- BenMAP/BenMap.cs
+++ BenMAP/BenMap.cs
@@ -2231,7 +2231,7 @@ namespace BenMAP
                     mainMap.Layers.Add((IMapLayer)TopMG1);
                     TopMG1.UnlockDispose();
                 }
-                mainMap.Layers.ResumeEvents();
+                //mainMap.Layers.ResumeEvents();
             }
             return 1; //if no result
         }
@@ -2983,7 +2983,6 @@ namespace BenMAP
         }
         private PolygonScheme CreateResultPolyScheme(ref MapPolygonLayer polLayer, int CategoryCount = 6, string isBase = "R")
         {
-            // 7-18-2017 - dpa - changes to handle no data values in symbology
             PolygonScheme myScheme1 = new PolygonScheme();
             myScheme1.EditorSettings.ClassificationType = ClassificationType.Quantities;
             myScheme1.EditorSettings.IntervalMethod = IntervalMethod.NaturalBreaks;
@@ -3020,22 +3019,8 @@ namespace BenMAP
                     break;
             }
 
-            // Create a copy of the datatable and remove the nodata elements from it. 
-            // Use the copy to make the scheme.
-            DataTable myDT = polLayer.DataSet.DataTable.Copy();
-            myDT.AcceptChanges();
-            foreach (DataRow myRow in myDT.Rows)
-            {
-                if (Convert.ToInt32(myRow[_columnName]) == -999)
-                {
-                    myRow.Delete();
-                }
-            }
-            myDT.AcceptChanges();
-            myScheme1.CreateCategories(myDT);
+            myScheme1.CreateCategories(polLayer.DataSet.DataTable);
 
-            // Remove the first category which is always the <0 one. And we don't want to show the -999 areas.
-            myScheme1.RemoveCategory(myScheme1.Categories[0]);
             for (int catNum = 0; catNum < myScheme1.Categories.Count; catNum++)
             {
                 myScheme1.Categories[catNum].Symbolizer.SetOutline(Color.Transparent, 0); //make the outlines invisble
@@ -11657,7 +11642,7 @@ namespace BenMAP
                             mainMap.ProjectionModeReproject = ActionMode.Never;
                             mainMap.ProjectionModeDefine = ActionMode.Never;
                             string shapeFileName = "";
-                            //mainMap.Layers.Clear();
+
                             if (CommonClass.GBenMAPGrid is ShapefileGrid)
                             {                               
                                 if (File.Exists(CommonClass.DataFilePath + @"\Data\Shapefiles\" + CommonClass.MainSetup.SetupName + "\\" + (CommonClass.GBenMAPGrid as ShapefileGrid).ShapefileName + ".shp"))
@@ -11673,7 +11658,6 @@ namespace BenMAP
                                 }
                             }
 
-                            //MapPolygonLayer CRResultMapPolyLayer = (MapPolygonLayer)mainMap.Layers.Add(shapeFileName);
                             MapPolygonLayer CRResultMapPolyLayer = (MapPolygonLayer)HIFResultsMapGroup.Layers.Add(shapeFileName);
 
                             DataTable dt = CRResultMapPolyLayer.DataSet.DataTable;
@@ -11725,31 +11709,37 @@ namespace BenMAP
                             {
                                 dicAll.Add(crcv.Col + "," + crcv.Row, crcv.PointEstimate);
                             }
-                            foreach (DataRow dr in dt.Rows)
-                                // 7/18/2017 - dpa - use -999 to indicate no data
+                                                   
+                            //make a list of no-data features and remove them before drawing - dpa - 8/15/2017
+                            List<int> IndicesToRemove = new List<int>();
+                            for (int q=0; q< dt.Rows.Count; q++)
                             {
                                 try
                                 {
+                                    DataRow dr = dt.Rows[q];
                                     if (dicAll.ContainsKey(dr[iCol] + "," + dr[iRow]))
-                                        dr["Incidence"] = dicAll[dr[iCol] + "," + dr[iRow]];
+                                       dr["Incidence"] = dicAll[dr[iCol] + "," + dr[iRow]];
                                     else
-                                        dr["Incidence"] = -999;
+                                        IndicesToRemove.Add(q);
                                 }
                                 catch (Exception ex)
                                 {
                                 }
                             }
+
                             if (File.Exists(CommonClass.DataFilePath + @"\Tmp\CRTemp.shp")) CommonClass.DeleteShapeFileName(CommonClass.DataFilePath + @"\Tmp\CRTemp.shp");
                             CRResultMapPolyLayer.DataSet.SaveAs(CommonClass.DataFilePath + @"\Tmp\CRTemp.shp", true);
-                            //mainMap.Layers.Clear();
-
-                            //CRResultMapPolyLayer = (MapPolygonLayer)mainMap.Layers.Add(CommonClass.DataFilePath + @"\Tmp\CRTemp.shp");
                             
                             MapPolygonLayer polLayer = CRResultMapPolyLayer;
                             polLayer.LegendText = author;
                             polLayer.Name = polLayer.LegendText;
                             string strValueField = polLayer.DataSet.DataTable.Columns[polLayer.DataSet.DataTable.Columns.Count - 1].ColumnName;
                             _columnName = strValueField;
+
+                            //remove all no-data features
+                            polLayer.RemoveFeaturesAt(IndicesToRemove);
+                            
+                            //build symbology 
                             polLayer.Symbology = CreateResultPolyScheme(ref polLayer, 6, "R"); //-MCB added
 
                             double dMinValue = 0.0;
@@ -11759,7 +11749,7 @@ namespace BenMAP
 
                             _dMinValue = dMinValue;
                             _dMaxValue = dMaxValue;
-                            //_currentLayerIndex = mainMap.Layers.Count - 1;
+
                             _CurrentIMapLayer = polLayer;
                             string pollutantUnit = string.Empty;
                             _columnName = strValueField;
