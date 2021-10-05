d1396906ca7b0ca801b22b644e4c74e2fd2f6cc3
YingziYang <yyang@indecon.com>
2021-10-05 14:36:09 -0400

    BENMAP-543. Instead of handling Percentiles fields in public void btnTableOutput_Click(object sender, EventArgs e), handle it in public static bool RunBatch(string strFile), variables cflsResult, IncidencelstResult and apvlstResult. Another update is to make command line also uses the new function for data export (_tableObject is Dictionary<AllSelectCRFunction, string>) so that it's consistent with UI and the result will include Pooling Name and Grid Definition.

--- BenMAP/BenMap.cs
+++ BenMAP/BenMap.cs
@@ -6045,6 +6045,7 @@ Color.FromArgb(255, 255, 166), 45.0F);
 							}
 
 						}
+
 						if ((apvlstResult == null || apvlstResult.Last().isChecked) && lstallSelectValuationMethodAndValue.First().lstAPVValueAttributes.First().LstPercentile != null)
 						{
 							i = 0;
@@ -6055,20 +6056,7 @@ Color.FromArgb(255, 255, 166), 45.0F);
 								i++;
 							}
 						}
-						else if (isBatch && lstallSelectValuationMethodAndValue.First().lstAPVValueAttributes.First().LstPercentile != null)
-						{
-							//BENMAP-543 in batch mode, apvlstResult.Last(), which is the field "percentile" is always unchecked but we would always want to include percentile in report.
-							i = 0;
-							while (i < lstallSelectValuationMethodAndValue.First().lstAPVValueAttributes.First().LstPercentile.Count())
-							{
-
-								dt.Columns.Add("Percentile " + ((Convert.ToDouble(i + 1) * 100.00 / Convert.ToDouble(lstallSelectValuationMethodAndValue.First().lstAPVValueAttributes.First().LstPercentile.Count()) - (100.00 / (2 * Convert.ToDouble(lstallSelectValuationMethodAndValue.First().lstAPVValueAttributes.First().LstPercentile.Count()))))), typeof(double));
-								i++;
-							}
-
-						}
-
-
+						
 						foreach (AllSelectValuationMethodAndValue allSelectValuationMethodAndValue in lstallSelectValuationMethodAndValue)
 						{
 							foreach (APVValueAttribute apvx in allSelectValuationMethodAndValue.lstAPVValueAttributes)
@@ -6112,6 +6100,7 @@ Color.FromArgb(255, 255, 166), 45.0F);
 									dr["Variance"] = apvx.Variance;
 
 								}
+								
 								else
 								{
 									foreach (FieldCheck fieldCheck in apvlstResult)
@@ -6133,19 +6122,7 @@ Color.FromArgb(255, 255, 166), 45.0F);
 										i++;
 									}
 								}
-								else if (isBatch && lstallSelectValuationMethodAndValue.First().lstAPVValueAttributes.First().LstPercentile != null)
-								{
-									//BENMAP-543 
-									i = 0;
-									while (i < lstallSelectValuationMethodAndValue.First().lstAPVValueAttributes.First().LstPercentile.Count())
-									{
-										dr["Percentile " + ((Convert.ToDouble(i + 1) * 100.00 / Convert.ToDouble(lstallSelectValuationMethodAndValue.First().lstAPVValueAttributes.First().LstPercentile.Count()) - (100.00 / (2 * Convert.ToDouble(lstallSelectValuationMethodAndValue.First().lstAPVValueAttributes.First().LstPercentile.Count())))))] = apvx.LstPercentile[i];
-										i++;
-									}
-
-								}
-
-
+								
 								dt.Rows.Add(dr);
 							}
 						}
